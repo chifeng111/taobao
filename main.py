8226354e5b47ae606134a945c59af853c6b598ae
zhenhua liao <zhenhua liao>
2017-02-22 14:44:22 +0800

    20170222

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from spider import parser
 
 if __name__ == '__main__':
     #获取100页源码列表
-    html = get_page("裙子")
+    html = get_page("手机")
+    #爬取商品名称，链接，价格
     for i in html:
         parser(i)
         print('-'*150)
