e5b879216c0fc900b38e290264be03e021fe43c5
zhenhua liao <zhenhua liao>
2017-02-24 18:17:39 +0800

    save to excel

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 __author__ = 'zhenhua'
 #coding:utf-8
+from openpyxl import Workbook, load_workbook
 from get_html import get_page
 from spider import parser
 
+#设置想爬取的内容
+content = '女裙'
+filename = content + '.xlsx'
+
+
 if __name__ == '__main__':
+    wb = Workbook()
+    ws = wb.active
     #获取100页源码列表
-    html = get_page("手机")
+    html = get_page(content)
     #爬取商品名称，链接，价格
+    ws.append(["商品名称", "链接地址", "价格"])
     for i in html:
-        parser(i)
-        print('-'*150)
+        data = parser(i)
+        for j in data:
+            ws.append(j)
+    wb.save(filename)
+    print("爬取成功，已保持在%s文件中" %filename)
--- spider.py
+++ spider.py
@@ -5,10 +5,13 @@ from bs4 import BeautifulSoup
 def parser(html):
     soup = BeautifulSoup(html, 'html.parser', from_encoding='gbk')
     item = soup.find_all('li', class_=' item item-border')
+    data = []
     for i in item:
         a = i.find('div', class_='title child-component').find('a')
         title = a.get_text()
         link = a['href']
         b = i.find('div', class_='price child-component clearfix').find('strong')
         price = b.get_text()
-        print("%s \t %s \t %s" %(title, link, price))
+        #print("%s \t %s \t %s" %(title, link, price))
+        data.append([title, link, price])
+    return data
